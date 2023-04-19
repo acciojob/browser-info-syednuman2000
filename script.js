421d0555a53b0718d4ba6960933c12a1cf25ca9d
Syed Numan <111847221+syednuman2000@users.noreply.github.com>
2023-04-19 11:45:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const line = document.getElementById("browser-info");
+
+line.innerText = "You are using ";
+line.innerText += navigator.appName;
+line.innerText += " version ";
+line.innerText += navigator.appVersion;
